7522ee8617fd8fef899d359bd0508fd81666d752
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-07-07 20:43:22 +0530

    sitemap-update

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -47,7 +47,8 @@ export const getServerSideProps = async ({ res }) => {
   ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+   
       ${staticPages
         .map((url) => {
           return `
