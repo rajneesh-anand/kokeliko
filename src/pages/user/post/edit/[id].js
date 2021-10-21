6fd049bab5535181a5cd91a32b56cb9815013732
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-10-21 22:25:48 +0530

    edit post -2

--- src/pages/user/post/edit/[id].js
+++ src/pages/user/post/edit/[id].js
@@ -13,6 +13,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import prisma from "@/libs/prisma";
 
 const Multiselect = dynamic(
   () =>
